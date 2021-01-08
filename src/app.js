35547e940af18426471438a102c7ddb6e7de1281
faiza203 <fiazfaiza981@gmail.com>
2021-01-08 10:41:39 -0500

    Add views path

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ require("./db/connect.js")
 
 const app = express();
 const port = process.env.PORT || 8000
+
 const staticPath = path.join(`${__dirname}/../public`)
+const viewsPath = path.join(`${__dirname}/../templates/views`);
 
 app.use(express.static(staticPath))
 app.set("view engine" , "hbs")
+app.set("views" , viewsPath)
 
 app.get("/" , (req , res) => {
     res.render('index')
